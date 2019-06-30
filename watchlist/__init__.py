31e6c5d3c164a49b047fea7b22f4cd81d10d0711
yangyuheng <1713361283@qq.com>
2019-06-30 12:42:42 +0800

    Ready to deploy.

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -15,9 +15,9 @@ else:
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']=prefix+os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_DATABASE_URI']=prefix+os.path.join(app.root_path,os.getenv('DATABASE_FILE','data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False   #关闭对模型修改的监控
-app.config['SECRET_KEY']='dev'                       #等同于 app.secret_key = 'dev'
+app.config['SECRET_KEY']=os.get('SECRET_KEY','dev')                       #等同于 app.secret_key = 'dev'
 
 db=SQLAlchemy(app)
 
